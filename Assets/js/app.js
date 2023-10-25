3b0a1355277e255a15b21345697bd5d403a50f19
Chrstjan <ps5christian@hotmail.com>
2023-10-25 08:18:41 +0200

    Made the pattern for the js validation

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -2,6 +2,9 @@ const name = document.getElementById("fName");
 const phoneNumber = document.getElementById("phoneNumber");
 const email = document.getElementById("Email");
 
+//This is the required patter for the email
+const emailRegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_´{|}~-]+@(?:\.[a-zA-Z0-9-]+)*$/;
+
 const form = document.getElementById("contact-form");
 const errorElement = document.getElementById("errorMessages");
 
@@ -10,6 +13,7 @@ form.addEventListener("submit", (e) => {
   if (name.value === "" || name.value == null) {
     messages.push("Navn skal udfyldes");
   }
+
 
   if (messages.length > 0) {
     e.preventDefault();
